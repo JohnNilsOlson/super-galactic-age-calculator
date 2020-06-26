e5ab1653ccc3ae63cc80a1b4a5b79b14978df1de
JohnNils Olson <johnnils@gmail.com>
2020-06-26 09:13:43 -0700

    fix typo, pass mercury object constructor test

--- __tests__/mercury.tests.js
+++ __tests__/mercury.tests.js
@@ -1,10 +1,9 @@
 import { Mercury } from './../src/mercury.js';
-import { TestScheduler } from 'jest';
 
 describe('mercury', () => {
 
   test('should correctly build mercury object', () => {
   let newMercury = new Mercury;
-  expect(newMurcury).toBeInstanceOf(Mercury);
+  expect(newMercury).toBeInstanceOf(Mercury);
   });
 });

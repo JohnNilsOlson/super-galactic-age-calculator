81f216990840a973fb89f233f4e271f45de6ef89
JohnNils Olson <johnnils@gmail.com>
2020-06-26 13:07:33 -0700

    add age input and earth age display functionality

--- src/index.js
+++ src/index.js
@@ -2,3 +2,16 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from "jquery";
+
+import { Mercury } from './../src/mercury.js';
+import { Venus } from './../src/venus.js';
+import { Mars } from './../src/mars.js';
+import { Jupiter } from './../src/jupiter.js';
+
+$(document).ready(function() {
+  $("#age").submit(function(event) {
+    event.preventDefault();
+    const age = $("input#inputted-age").val();
+    $("#earth-age").text(age);
+  });
+});

45408fa5e7bf1a2069dafaba769f830c6d6f8c9d
JohnNils Olson <johnnils@gmail.com>
2020-06-26 10:14:39 -0700

    add failing test for mars relative age calculation

--- __tests__/mars.tests.js
+++ __tests__/mars.tests.js
@@ -6,4 +6,10 @@ describe('mars', () => {
     let newMars = new Mars;
     expect(newMars).toBeInstanceOf(Mars);
   });
+
+  test('should correctly calculate relative age on mercury', () => {
+    let newMars = new Mars(33);
+    newMars.calcAge();
+    expect(newMars.relativeAge).toEqual(62);
+  });
 });

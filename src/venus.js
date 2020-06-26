72c5d567cc16407de9e04cf525c967ce85058a39
JohnNils Olson <johnnils@gmail.com>
2020-06-26 09:54:18 -0700

    add calcAge functon to pass test for venus relative age calculation

--- src/venus.js
+++ src/venus.js
@@ -1,9 +1,10 @@
 export class Venus {
   constructor(age, relativeYear, relativeAge) {
+    this.age = age;
     this.relativeYear = .62;
   }
 
   calcAge() {
-    
+    this.relativeAge = Math.round(this.age * this.relativeYear);
   }
 }

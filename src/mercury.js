c1a72d8ac934468f83c96618990a4a1cc9b7c813
JohnNils Olson <johnnils@gmail.com>
2020-06-26 09:27:21 -0700

    add calcAge function to pass test for relative age calculation

--- src/mercury.js
+++ src/mercury.js
@@ -1,10 +1,10 @@
 export class Mercury {
-  constructor(age, relativeYear) {
+  constructor(age, relativeYear, relativeAge) {
     this.age = age;
     this.relativeYear = .24;
   }
 
   calcAge() {
-    
+    this.relativeAge = Math.round(this.age * this.relativeYear);
   }
 }

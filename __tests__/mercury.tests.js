faf51e877d2a9a218a0121fb37e55e3d8e3ba270
JohnNils Olson <johnnils@gmail.com>
2020-06-26 09:19:31 -0700

    add failing test for relative age calculation

--- __tests__/mercury.tests.js
+++ __tests__/mercury.tests.js
@@ -6,4 +6,10 @@ describe('mercury', () => {
   let newMercury = new Mercury;
   expect(newMercury).toBeInstanceOf(Mercury);
   });
+
+  test('should correctly calculate relative age on mercury', () => {
+  let newMercury = new Mercury(33);
+  newMercury.calcAge();
+  expect(newMercury.relativeAge).toEqual(8);
+  });
 });
